3a4201189b41e0ac7c9ae534c4724782d1d51c9f
pradeep-18-bit <akurathipradeep14@gmail.com>
2025-11-28 15:08:22 +0530

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "sri")
-option_b = os.getenv('OPTION_B', "harsha")
+option_a = os.getenv('OPTION_A', "fun day")
+option_b = os.getenv('OPTION_B', "early home")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
